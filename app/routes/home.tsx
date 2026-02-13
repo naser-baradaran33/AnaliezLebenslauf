50c05bb626a2121cf0b15e1a11c361d34486e35a
daniel <danielbaradaran31-hash>
2026-02-13 11:50:54 +0100

    section components teyps

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -17,22 +17,22 @@ export default function Home() {
     
     
     <section className="main-section">
-      <div className="page-heading">
+      <div className="page-heading py-16">
         <h1>Track Your Applications & Resume Ratings</h1>
         <h2>Review your submissions and check AI-power feedback.</h2>
 
       </div>
 
-        {resumes.length > 0 && (
+
+
+    {resumes.length > 0 && (
       <div className="resumes-section">
         {resumes.map((resume) => (
           <ResumeCard key={resume.id} resume={resume} />
         ))}
       </div>
     )}
-
-    </section>
-    
   
+     </section>
   </main>
 }
